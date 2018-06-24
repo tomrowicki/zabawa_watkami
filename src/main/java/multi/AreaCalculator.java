819afd4bf54da7a1b20bc7b67c52befc986d3943
Tomasz <tomrowicki@gmail.com>
2018-06-24 19:53:24 +0200

    - uruchamianie równoległe wszystkich wątków - losowanie argumentów, wyliczać wynik w teście i wpuszczać liczby do komponentu, porównując z wynikiem - sleep z losową ilością ms dla każdego wątku - druga metoda, stanowa, pokazująca jak nie należy robić

--- src/main/java/multi/AreaCalculator.java
+++ src/main/java/multi/AreaCalculator.java
@@ -5,8 +5,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class AreaCalculator {
 
-	public int calculateRectangleArea(Integer a, Integer b) {
+	private Integer dangerousField1;
+
+	private Integer dangerousField2;
+
+	private Integer dangerousField3;
+
+	public Integer calculateRectangleArea(Integer a, Integer b) {
 		Integer c = a * b;
 		return c;
+	}
+	
+	public Integer wronglyCalculateRectangleAreaInParallel(Integer a, Integer b) {
+		this.dangerousField1 = a;
+		this.dangerousField2 = b;
+		this.dangerousField3 = this.dangerousField1 * this.dangerousField2;
+		return this.dangerousField3;
 	}
 }
--- src/test/java/multi/AreaCalculatorTest.java
+++ src/test/java/multi/AreaCalculatorTest.java
@@ -2,6 +2,11 @@ package multi;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,45 +17,54 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AreaCalculatorTest {
 
-	// TODO executor
-	// TODO losowanie argumentów, wyliczać wynik w teście i wpuszczać liczby do
-	// komponentu, porównując z wynikiem
-	// TODO sleep z losową ilością ms dla każdego wątku
-	// TODO druga metoda, stanowa, pokazująca jak nie należy robić
-	
 	// korzysta ze springowego singletona
 	@Autowired
 	private AreaCalculator areaCalculator;
+
+	private Random randomizer = new Random(1234);
 
 	@Test
 	public void shouldProperlyCalculateArea() {
 		// 1000 powtórzeń
 		for (int i = 0; i < 1000; i++) {
-			test();
+			calculateAreaInParallel(true);
+		}
+	}
+	
+	// tutaj gwałcimy założenia thread-safe, więc asercja może nie pyknąć
+	@Test (expected = AssertionError.class)
+	public void shouldMessUpCalculatingArea() {
+		for (int i = 0; i < 1000; i++) {
+			calculateAreaInParallel(false);
 		}
 	}
 
-	private void test() {
-		Runnable[] threadPool = new Runnable[8];
+	private void calculateAreaInParallel(boolean doItProperly) {
+		List<Runnable> threadPool = new ArrayList<Runnable>();
 		// dla procesora 8-wątkowego
 		for (int i = 0; i < 8; i++) {
+			// wyliczamy uprzednio poprawny wynik, przed inicjalizacją wątków
+			int arg1 = randomizer.nextInt(8) + 1;
+			int arg2 = randomizer.nextInt(8) + 1;
+			Integer area = arg1 * arg2;
+			
 			Runnable t;
-			// różne argumenty dla parzystych/nieparzystych wątków
-			if (i % 2 == 0) {
-				t = () -> {
-					assertEquals(6, areaCalculator.calculateRectangleArea(2, 3));
-				};
-
-			} else {
-				t = () -> {
-					assertEquals(12, areaCalculator.calculateRectangleArea(3, 4));
-				};
-			}
-			threadPool[i] = t;
+			t = () -> {
+				// różne wątki wykonują się z różną prędkością
+				try {
+					TimeUnit.MILLISECONDS.sleep(randomizer.nextInt(10));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (doItProperly) {
+					assertEquals(area, areaCalculator.calculateRectangleArea(arg1, arg2));
+				} else {
+					assertEquals(area, areaCalculator.wronglyCalculateRectangleAreaInParallel(arg1, arg2));
+				}
+			};
+			threadPool.add(t);
 		}
-		// odpalenie wszystkich wątków
-		for (Runnable thread : threadPool) {
-			thread.run();
-		}
+		// odpalenie wszystkich wątków równolegle
+		threadPool.parallelStream().forEach(Runnable::run);
 	}
 }
